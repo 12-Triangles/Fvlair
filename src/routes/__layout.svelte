0ff3f5e6178657e37bf72fc75f8e0d47c0255758
Tyler Kuster <tylerkuster@gmail.com>
2022-06-03 16:45:47 -0500

    lots of home page stuff

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,233 @@
+<script lang="ts">
+  // import MediaQuery from './MediaQuery.svelte';
+  import { onMount, beforeUpdate } from 'svelte'
+  //   import { MetaTags } from 'svelte-meta-tags'
+  //   export let isHome = false
+  //   beforeUpdate(() => {
+  //     if (location !== undefined) {
+  //       isHome =
+  //         location.href === 'http://localhost:3000/' ||
+  //         location.href === 'https://ionhipster.com/'
+  //     }
+  //   })
+</script>
+
+<style>
+  .slot {
+    max-width: 96.25%;
+    width: 2000px;
+    margin: auto;
+  }
+
+  .container {
+    max-width: 96.25%;
+    width: 2000px;
+    margin: auto;
+  }
+
+  .nav-bar {
+    width: 100%;
+    text-transform: uppercase;
+    justify-content: space-between;
+    background-color: #222;
+    height: 42px;
+    z-index: 2;
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: flex;
+  }
+
+  .home-button {
+    background: linear-gradient(219.01deg, #eb6c85 0%, #c265e8 100%);
+    padding: 12px;
+    color: #f4eaec;
+    font-weight: 700;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .links {
+    color: white;
+    font-weight: 700;
+    height: 42px;
+    display: flex;
+  }
+
+  a {
+    color: #fff !important;
+    background-color: #222;
+    text-decoration: none;
+  }
+
+  a:hover {
+    background-color: #444;
+    text-shadow: none;
+  }
+
+  .link {
+    padding: 12px;
+    display: flex;
+    flex-direction: column;
+  }
+</style>
+
+<div class="container">
+  <!-- <nav class="flex cen">
+    <div>&nbsp;&nbsp;&nbsp;</div>
+    <div class="flex col">
+      <a href="/">
+        <img
+          class="header-img"
+          src="/ionHipsterMustache.svg"
+          alt="ionhipster" />
+      </a>
+      <div class="spacer">&nbsp;</div>
+    </div>
+    <div class="spacer">&nbsp;</div>
+    <div class="tagline">
+      {#if isHome}
+        <b>ionhipster</b>
+        is a journal of notes, tips, and tricks on Ionic development.
+        <div class="spacer">&nbsp;</div>
+      {/if}
+      {#if !isHome}
+        <div class="spacer">&nbsp;</div>
+      {/if}
+    </div>
+    <div class="spacer">&nbsp;</div>
+  </nav> -->
+  <br />
+  <br />
+  <br />
+  <div class="slot">
+    <slot />
+  </div>
+</div>
+<!-- <MetaTags
+  title="ionhipster"
+  description="A journal of notes, tips, and tricks on Ionic development." />
+
+<br />
+<br />
+<MediaQuery query="(min-width: 700px)" let:matches>
+  {#if matches}
+    {#if !isHome}
+      <br />
+    {/if}
+
+    <div class="container">
+      <nav class="flex cen">
+        <div>&nbsp;&nbsp;&nbsp;</div>
+        <div class="flex col">
+          <a href="/">
+            <img
+              class="header-img"
+              src="/ionHipsterMustache.svg"
+              alt="ionhipster" />
+          </a>
+          <div class="spacer">&nbsp;</div>
+        </div>
+        <div class="spacer">&nbsp;</div>
+        <div class="tagline">
+          {#if isHome}
+            <b>ionhipster</b>
+            is a journal of notes, tips, and tricks on Ionic development.
+            <div class="spacer">&nbsp;</div>
+          {/if}
+          {#if !isHome}
+            <div class="spacer">&nbsp;</div>
+          {/if}
+        </div>
+        <div class="spacer">&nbsp;</div>
+      </nav>
+      <br />
+      <br />
+      <br />
+      <div class="slot">
+        <slot />
+      </div>
+    </div>
+  {/if}
+</MediaQuery>
+<MediaQuery query="(max-width: 699px)" let:matches>
+  {#if matches}
+    <div class="container-mobile">
+      <nav class="flex col cen">
+        <div class="spacer">&nbsp;</div>
+        <a href="/">
+          <img
+            class="header-img"
+            src="/ionHipsterMustache.svg"
+            alt="ionhipster" />
+        </a>
+        {#if isHome}
+          <div class="spacer">&nbsp;</div>
+        {/if}
+        <div class="tagline-mobile">
+          {#if isHome}
+            <b>ionhipster</b>
+            is a journal of notes, tips, and tricks on Ionic development.
+          {/if}
+        </div>
+      </nav>
+      <br />
+      <br />
+      <br />
+      <div class="slot">
+        <slot />
+      </div>
+    </div>
+  {/if}
+</MediaQuery> -->
+<div class="nav-bar">
+  <a href="/" class="home-button">12 Triangles</a>
+  <div class="links">
+    <a class="link" href="https://sayitwithflair.com">Flair</a>
+    <a class="link" href="https://ionhipster.com">Blog</a>
+  </div>
+</div>
+<br />
+<br />
+<br />
+<!-- <div class="license">
+  <img
+    aria-hidden="true"
+    class="cc"
+    src="/cc.svg"
+    title="Creative Commons"
+    alt="Creative Commons" />
+  <img
+    aria-hidden="true"
+    class="cc"
+    src="/by.svg"
+    title="Attribution"
+    alt="Attribution" />
+  <img
+    aria-hidden="true"
+    class="cc"
+    src="/nc.svg"
+    title="Non-Commercial"
+    alt="Non-Commercial" />
+  ionhipster.com is released under a
+  <a href="https://creativecommons.org/licenses/by-nc/4.0/" rel="license">
+    Creative Commons BY-NC License
+  </a>
+  .
+</div>
+<br />
+<br />
+<div class="flex cen">
+  <div class="spacer">&nbsp;</div>
+  <a href="/">
+    <img class="footer-image" src="ionHipsterMustache.svg" alt="ionhipster" />
+  </a>
+  <br />
+  <a href="https://12triangles.com">
+    <img class="footer-image" src="/12TrianglesDark.svg" alt="12 Triangles" />
+  </a>
+  <div class="spacer">&nbsp;</div>
+</div>
+<br /> -->
+<div class="copyright">© 12 Triangles, LLC 2022</div>
+<br />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,131 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit
-  <a href="https://kit.svelte.dev">kit.svelte.dev</a>
-  to read the documentation
-</p>
+<style>
+  :global(body) {
+    margin: 0px;
+    border-top: 8px solid #ff8438;
+    background-color: #6f1f8f;
+    color: #222222;
+    /* font-size: 21px; */
+    font-family: 'Inter', sans-serif;
+  }
+  .card-container {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    max-width: 1200px;
+    width: 100%;
+    margin: auto;
+    position: relative;
+    z-index: 1;
+    border: 1px solid blue;
+  }
+
+  .big-logo {
+    margin: 48px auto;
+    max-width: 200px;
+    filter: drop-shadow(32px 32px 80px #00000010);
+  }
+
+  .flair-text {
+    text-align: center;
+    font-size: 32px;
+    margin: auto;
+
+    max-width: 680px;
+    width: 96.25%;
+    color: rgb(255, 255, 255);
+  }
+
+  .container-text {
+    color: white;
+    font-size: 20px;
+    font-weight: 700;
+    margin: 16px 0px 80px;
+  }
+
+  .card-list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    margin: auto 16px;
+
+    border: 1px solid red;
+  }
+
+  .card {
+    width: 96.25%;
+    max-width: 480px;
+    height: auto;
+  }
+  @media (max-width: 500px) {
+    .flair-text {
+      font-size: 24px;
+    }
+
+    .container-text {
+      font-size: 16px;
+      font-weight: 400;
+    }
+  }
+</style>
+
+<br />
+<br />
+<div class="card-container">
+  <a href="/">
+    <img class="big-logo" src="static/flairLogo.svg" alt="Flair Logo" />
+  </a>
+  <!-- <img class="big-text" src="assets/comingSoon.svg" alt="Coming Soon Text" /> -->
+  <!-- <br /> -->
+  <div class="flair-text">
+    Flair makes it easy to create customized stickers to personalize your text
+    and photo messages.
+  </div>
+  <br />
+  <br />
+  <br />
+  <br />
+
+  <div class="card-list">
+    <div>
+      <img class="card" src="static/themes-feature.svg" alt="" />
+      <div class="container-text">Popular Themes, Specially Picked For You</div>
+    </div>
+    <div>
+      <img class="card" src="static/stickers-feature.svg" alt="" />
+      <div class="container-text">Make Your Own Stickers and Sticker Packs</div>
+    </div>
+    <div>
+      <img class="card" src="static/tools-feature.svg" alt="" />
+      <div class="container-text">Intuitive Graphic Design Tools</div>
+    </div>
+    <div>
+      <img class="card" src="static/share-feature.svg" alt="" />
+      <div class="container-text">Share Your Stickers Anywhere!</div>
+    </div>
+  </div>
+  <br />
+  <div class="flex-me quick-links-flair">
+    <a href="/support">Tech Support</a>
+    &nbsp;&nbsp;|&nbsp;&nbsp;
+    <a href="/flair-about">Flair FAQ</a>
+    &nbsp;&nbsp;|&nbsp;&nbsp;
+    <a href="/terms">Terms of Service</a>
+    &nbsp;&nbsp;|&nbsp;&nbsp;
+    <a href="/privacy">Data Privacy</a>
+  </div>
+
+  <div class="flex-me small-logo">
+    <a href="/flair">
+      <img class="small-logo" src="assets/flairLogo.svg" alt="flair logo" />
+    </a>
+    <a href="">
+      <img
+        class="small-logo"
+        src="assets/12TrianglesWhite.svg"
+        alt="12 Triangles logo" />
+    </a>
+  </div>
+  <br />
+  <div class="contact">FLAIR IS A PRODUCT OF 12 TRIANGLES</div>
+</div>
+<br />
