b8e40b6ba35eba6ee8ad2afe024a265bc984754c
Tyler Kuster <tylerkuster@gmail.com>
2024-05-26 23:31:47 -0500

    add coming sooon bit

--- src/components/themes.svelte
+++ src/components/themes.svelte
@@ -1,67 +1,67 @@
 <h1>Flair Texture Library</h1>
 <div class="feature-link-group">
   <a href="/acrylic">
-    <div class="hover-effect flair" />
+    <div class="hover-effect soon" />
     <div class="title">acrylic</div>
   </a>
   <a href="/black-ink">
-    <div class="hover-effect flair" />
+    <div class="hover-effect soon" />
     <div class="title">black ink</div>
   </a>
   <a href="/collage-paper">
-    <div class="hover-effect flair" />
+    <div class="hover-effect soon" />
     <div class="title">collage paper</div>
   </a>
   <a href="/colorful-foil">
-    <div class="hover-effect flair" />
+    <div class="hover-effect soon" />
     <div class="title">colorful foil</div>
   </a>
   <a href="/colorful-marble">
-    <div class="hover-effect flair" />
+    <div class="hover-effect soon" />
     <div class="title">colorful marble</div>
   </a>
   <a href="/gold foil">
-    <div class="hover-effect flair" />
+    <div class="hover-effect soon" />
     <div class="title">gold foil</div>
   </a>
   <a href="/gold-marble">
-    <div class="hover-effect flair" />
+    <div class="hover-effect soon" />
     <div class="title">gold marble</div>
   </a>
   <a href="/gold-paper">
-    <div class="hover-effect flair" />
+    <div class="hover-effect soon" />
     <div class="title">gold paper</div>
   </a>
   <a href="/gradients">
-    <div class="hover-effect flair" />
+    <div class="hover-effect soon" />
     <div class="title">gradients</div>
   </a>
   <a href="/halftone">
-    <div class="hover-effect flair" />
+    <div class="hover-effect soon" />
     <div class="title">halftone</div>
   </a>
   <a href="/holographic">
-    <div class="hover-effect flair" />
+    <div class="hover-effect soon" />
     <div class="title">holographic</div>
   </a>
   <a href="/mixed-media">
-    <div class="hover-effect flair" />
+    <div class="hover-effect soon" />
     <div class="title">mixed media</div>
   </a>
   <a href="/pastel">
-    <div class="hover-effect flair" />
+    <div class="hover-effect soon" />
     <div class="title">pastel</div>
   </a>
   <a href="/pretty-textures">
-    <div class="hover-effect flair" />
+    <div class="hover-effect soon" />
     <div class="title">pretty textures</div>
   </a>
   <a href="/retro">
-    <div class="hover-effect flair" />
+    <div class="hover-effect soon" />
     <div class="title">retro</div>
   </a>
   <a href="/watercolor">
-    <div class="hover-effect flair" />
+    <div class="hover-effect watercolor" />
     <div class="title">Watercolor</div>
   </a>
 </div>
@@ -94,8 +94,14 @@
     transition: all 0.25s;
   }
 
-  .flair {
+  .watercolor {
     background-image: url("/watercolor.jpg");
+    background-position: center;
+    background-size: cover;
+  }
+
+  .soon {
+    background-image: url("/soon.jpg");
     background-position: center;
     background-size: cover;
   }
